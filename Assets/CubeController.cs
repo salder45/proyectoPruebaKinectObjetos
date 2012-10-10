812156b3661eef7aebcd8afcfeb3fdeae94893aa
Eder <edermtz.04@gmail.com>
2012-10-09 20:45:18 -0500

    usando exclusivamente el skeleton tracking

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,10 +10,8 @@ public class CubeController : MonoBehaviour {
 	private Context context;
 	private ScriptNode scriptNode;
 	private DepthGenerator depth;
-	private HandsGenerator hands;
-	private GestureGenerator gesture;
-	private Dictionary<int,List<Point3D>> tracking;
-	private int trackingSize = 10;
+	
+	private Point3D puntoTorso;
 	
 	
 	
@@ -25,7 +23,13 @@ public class CubeController : MonoBehaviour {
 	private bool shouldRun;
 	private Dictionary <int, Dictionary<SkeletonJoint,SkeletonJointPosition>> joints;
 	//
-
+	public float valorRotation=0.75f;
+	private int manoDr=0;
+	private int manoIz=0;
+	private Point3D DrRespaldo;
+	private Point3D IzRespaldo;
+	
+	
 	void Start () {
 		Debug.Log("START APP");
 		this.context=Context.CreateFromXmlFile(XML_CONFIG, out scriptNode);
@@ -33,25 +37,7 @@ public class CubeController : MonoBehaviour {
 		if(depth==null){
 			throw new Exception("Nodo de Profundidad no encontrado");
 		}
-		this.hands=this.context.FindExistingNode(NodeType.Hands)as HandsGenerator;
-		if(this.hands==null){
-			throw new Exception("Nodo de Manos no encontrado");
-		}
-		this.gesture=this.context.FindExistingNode(NodeType.Gesture)as GestureGenerator;
-		if(this.gesture==null){
-			throw new Exception("Nodo de Gestos no encontrado");
-		}
-		//Agregar Handlers
-		this.hands.HandCreate+=hands_HandCreate;
-		this.hands.HandUpdate+=hands_HandUpdate;
-		this.hands.HandDestroy+=hands_HandDestroy;
-		
-		
-		this.gesture.AddGesture("Wave");
-		this.gesture.GestureRecognized+=gesture_GestureRecognized;
-		this.gesture.StartGenerating();
-		tracking=new Dictionary<int, List<Point3D>>();
-		
+	
 		
 		//User Tracking
 		this.userGenerator=new UserGenerator(this.context);
@@ -64,7 +50,6 @@ public class CubeController : MonoBehaviour {
 		this.skeletonCapability.CalibrationComplete+=skeletonCapability_CalibrationComplete;		
 		this.skeletonCapability.SetSkeletonProfile(SkeletonProfile.All);
 		this.userGenerator.StartGenerating();
-		this.shouldRun=true;
 		this.joints=new Dictionary<int,Dictionary<SkeletonJoint,SkeletonJointPosition>>();
 		//
 	}
@@ -72,81 +57,27 @@ public class CubeController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("Update");
-		
+		//Debug.Log("Update");	
 		
 		this.context.WaitOneUpdateAll (this.depth);
-		calcular();
-		
 		int[] users=this.userGenerator.GetUsers();
 			foreach(int user in users){
 				if(this.skeletonCapability.IsTracking(user)){
-				SkeletonJointPosition punto=this.skeletonCapability.GetSkeletonJointPosition(user,SkeletonJoint.Torso);
-				Debug.Log("Punto Medio es: X"+punto.Position.X+" Y: "+punto.Position.Y+" Z: "+punto.Position.Z);
+				updatePuntoRef(skeletonCapability.GetSkeletonJointPosition(user,SkeletonJoint.Torso));
+				Point3D handIz,handDr;
+				SkeletonJointPosition sjpHandIz=skeletonCapability.GetSkeletonJointPosition(user,SkeletonJoint.LeftHand);
+				SkeletonJointPosition sjpHandDr=skeletonCapability.GetSkeletonJointPosition(user,SkeletonJoint.RightHand);
+				if(isDentroDelCuadro(sjpHandDr.Position)&&isDentroDelCuadro(sjpHandIz.Position)){
+					Debug.Log("Dentro del cuadro");
+				}
 			}
 		}
-		
-		
-		/*
-		List<int> keys = new List<int>(tracking.Keys);
-		foreach(int key in keys){
-			//Debug.Log(key);
-			List<Point3D> l=tracking[key];
-			Debug.Log("X "+l[l.Count-1].X+" Y "+l[l.Count-1].Y+" Z "+l[l.Count-1].Z);
-		}
-		*/
 	}
 	
 	void OnApplicationQuit(){
 		Debug.Log("Saliendo de la aplicacion");
 		context.Release();
-	}
-	
-	//Handlers-START
-	void gesture_GestureRecognized(object sender, GestureRecognizedEventArgs e){
-		Debug.Log("Algo --> "+e.Gesture);
-		if(e.Gesture=="Wave"){
-			this.hands.StartTracking(e.EndPosition);
-			Debug.Log("Tracking ");
-		}
-	}
-	
-	void hands_HandCreate(object sender, HandCreateEventArgs e){
-		Debug.Log("Create");
-		List<Point3D> lista=new List<Point3D>(trackingSize);
-		lista.Add(e.Position);
-		tracking.Add(e.UserID,lista);
-		
-	}
-	
-	void hands_HandUpdate(object sender, HandUpdateEventArgs e){
-		Debug.Log("Update");
-		List<Point3D> lista=tracking[e.UserID];
-		lista.Add(e.Position);
-		if(lista.Count>trackingSize){
-			lista.RemoveAt(0);
-		}
-	}
-	
-	void hands_HandDestroy(object sender,HandDestroyEventArgs e){
-		Debug.Log("Destroy");
-		tracking.Remove(e.UserID);
-	}
-	
-	void calcular(){
-		List<int> keys = new List<int>(tracking.Keys);
-		if(keys.Count==2){
-			
-			
-			foreach(int key in keys){
-				
-			}			
-		}else{
-			Debug.Log("No se puede mover");
-		}
-	}
-	//Handlers-END
-	
+	}	
 	
 	
 	//User Generator Handlers
@@ -180,4 +111,27 @@ public class CubeController : MonoBehaviour {
 		this.joints.Remove(e.ID);
 	}
 	
+	void updatePuntoRef(SkeletonJointPosition punto){
+		this.puntoTorso=punto.Position;
+	}
+	
+	
+	bool isDentroDelCuadro(Point3D punto){
+		float xCentro=puntoTorso.X;
+		float yCentro=puntoTorso.Y;
+		float zCentro=puntoTorso.Z;
+		bool dentro=false;
+		if((punto.X<xCentro+200f&&punto.X>xCentro-200f)&&(punto.Y<yCentro+200f&&punto.Y>yCentro-200f)&&(punto.Z>zCentro-450f&&punto.Z<zCentro-50f)){
+			dentro=true;
+		}		
+		return dentro;
+	}
+	
+	bool isManoDr(Point3D mano){
+		bool der=false;
+		if(mano.X<puntoTorso.X){
+			der=true;
+		}		
+		return der;
+	}
 }
